ad947564555442683c450130bb936b59b43d4655
Gamaliel Leon <e-gentrambasaguas@neoris.com>
2020-12-07 23:47:04 -0600

    Add an example using ngClass and ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'CRUD';
+  image: string = 'https://i.blogs.es/594843/chrome/450_1000.jpg';
+  currentOption: number = 0;
+  band: boolean = false;
+  setOption(option: number, band: boolean): void{
+    this.currentOption = option;
+    this.band = band;
+  }
 }
+
+
